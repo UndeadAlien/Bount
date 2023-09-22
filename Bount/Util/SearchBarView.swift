b5aad88af21cdda5f3b8ca4c13e47fe56635a229
Connor Hutchinson <BusinessHutch@gmail.com>
2023-09-22 04:32:25 -0400

    Added some styling to the search bar

--- Bount/Util/SearchBarView.swift
+++ Bount/Util/SearchBarView.swift
@@ -14,22 +14,32 @@ struct SearchBarView: View {
         HStack {
             TextField("Search", text: $text)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-
-            Button(action: {
-                text = ""
-            }) {
-                Image(systemName: "xmark.circle.fill")
-                    .foregroundColor(.gray)
-                    .padding(.trailing)
-            }
+                .padding(.horizontal, 10)
+                .cornerRadius(10)
+                .shadow(radius: 2)
+                .font(.title2)
+                .autocorrectionDisabled(true)
+                .autocapitalization(.none)
+                .overlay {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            text = ""
+                        }) {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.gray)
+                                .font(.title2)
+                                .padding(.trailing)
+                        }
+                    }
+                }
         }
     }
 }
 
 struct SearchBarView_Previews: PreviewProvider {
     
-    @State static var searchText: String = "Enter Query Here"
+    @State static var searchText: String = "Search"
     
     static var previews: some View {
         SearchBarView(text: $searchText)
